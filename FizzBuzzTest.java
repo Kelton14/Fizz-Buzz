50136856e0ec5272b3cac02b118add596ecf3288
Kelton Day <keltonday@yahoo.com>
2021-06-18 10:19:20 -0500

    initial commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz f = new FizzBuzz();
+        f.fizzBuzz(15);
+        f.fizzBuzz(5);
+        f.fizzBuzz(3);
+        System.out.println(f.fizzBuzz(2));
+    }
+}
